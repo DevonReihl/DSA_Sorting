5bdeda777e40e2a6d1f04a2da10f7d12177323c4
Devon Reihl <devon.reihl@gmail.com>
2020-11-05 10:13:23 -0800

    quick sort problem 2

--- sort.js
+++ sort.js
@@ -38,8 +38,8 @@ function merge(left, right, array) {
     return array;
 }
 
-let myArray = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
-mergeSort(myArray)
+//let myArray = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
+//mergeSort(myArray)
 
 //1.1 3 calls we have split the left side values all the way to individual element lists on two list of one element
 
@@ -51,17 +51,92 @@ mergeSort(myArray)
 //1.4 after 7 merges we have the entire left side sorted from initial split.
 
 
-
 //2 Quick sort
-//2.1
+function quickS(array, start = 0, end = array.length) {
+    if (start >= end) {
+        return array;
+    }
 
+    const middle = partition(array, start, end);
+    array = quickSort(array, start, middle);
+    array = quickSort(array, middle + 1, end);
+    return array;
+};
+
+function partition(array, start, end) {
+    let tempItem = array.shift();
+    array.push(tmpItem);
+    console.log(array);
+    const pivot = array[end - 1];
+    let j = start;
+    for (let i = start; i < end - 1; i++) {
+        if (array[i] <= pivot) {
+            swap(array, i, j);
+            j++;
+        }
+    }
+    swap(array, end-1, j);
+    count++;
+    console.log('COUNT: --- ', count, 'ARRAY: -------', array)
+    return j;
+};
+
+function swap(array, i, j) {
+    const tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+};
+
+let myArr =[14, 17, 13, 15, 19, 10, 3, 16, 9, 12]
+console.log(quickSort(myArr));
+
+//2.1 the pivot could have been either 14 or 17
+  //Pivot 12: [3, 9, 10, 12, 19, 14, 17, 16, 13, 15]
 
 //2.2
 
 
 
-//3 Implementing quicksort
+//Pivot 14: [14, 13, 10]
 
+
+//3 Implementing quicksort
+function quickSort(array, start = 0, end = array.length) {
+  if (start >= end) {
+      return array;
+  }
+
+  const middle = partition(array, start, end);
+  array = quickSort(array, start, middle);
+  array = quickSort(array, middle + 1, end);
+  return array;
+};
+
+function partition(array, start, end) {
+  array.shift();
+  array.push();
+  const pivot = array[end - 1];
+  let j = start;
+  for (let i = start; i < end - 1; i++) {
+      if (array[i] <= pivot) {
+          swap(array, i, j);
+          j++;
+      }
+  }
+  swap(array, end-1, j);
+  count++;
+  console.log('COUNT: --- ', count, 'ARRAY: -------', array)
+  return j;
+};
+
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+};
+
+let myArr =[14, 17, 13, 15, 19, 10, 3, 16, 9, 12]
+console.log(quickSort(myArr));
 
 
 
